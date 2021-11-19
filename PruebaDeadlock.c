8036e1aa1a67064162c2291e7a3b29eb5c51c3d9
tomasferraro <tomas.ferraro@mercadolibre.com>
2021-11-19 11:36:04 -0300

    Se comio fiero la identacion

--- PruebaDeadlock.c
+++ PruebaDeadlock.c
@@ -19,20 +19,20 @@ void* carpincho1_func(void* config){
 	printf("C1 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM1", 1);
-  mate_sem_init(&instance, "SEM2", 1);
+    mate_sem_init(&instance, "SEM1", 1);
+    mate_sem_init(&instance, "SEM2", 1);
 
-  mate_sem_wait(&instance, "SEM1");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM2");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM1");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM2");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM1");
-  mate_sem_post(&instance, "SEM2");
+    mate_sem_post(&instance, "SEM1");
+    mate_sem_post(&instance, "SEM2");
 
-	printf("C2 - Se retira a descansar\n");
-	mate_close(&instance);
-	return 0;
+    printf("C2 - Se retira a descansar\n");
+    mate_close(&instance);
+    return 0;
 }
 
 void* carpincho2_func(void* config){
@@ -41,16 +41,16 @@ void* carpincho2_func(void* config){
 	printf("C2 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM3", 1);
-  mate_sem_init(&instance, "SEM4", 1);
+    mate_sem_init(&instance, "SEM3", 1);
+    mate_sem_init(&instance, "SEM4", 1);
 
-  mate_sem_wait(&instance, "SEM2");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM3");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM2");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM3");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM2");
-  mate_sem_post(&instance, "SEM3");
+    mate_sem_post(&instance, "SEM2");
+    mate_sem_post(&instance, "SEM3");
 
 	printf("C2 - Se retira a descansar\n");
 	mate_close(&instance);
@@ -63,16 +63,16 @@ void* carpincho3_func(void* config){
 	printf("C3 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM3", 1);
-  mate_sem_init(&instance, "SEM4", 1);
+    mate_sem_init(&instance, "SEM3", 1);
+    mate_sem_init(&instance, "SEM4", 1);
 
-  mate_sem_wait(&instance, "SEM3");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM4");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM3");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM4");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM3");
-  mate_sem_post(&instance, "SEM4");
+    mate_sem_post(&instance, "SEM3");
+    mate_sem_post(&instance, "SEM4");
 
 	printf("C3 - Se retira a descansar\n");
 	mate_close(&instance);
@@ -85,16 +85,16 @@ void* carpincho4_func(void* config){
 	printf("C4 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM1", 1);
-  mate_sem_init(&instance, "SEM4", 1);
+    mate_sem_init(&instance, "SEM1", 1);
+    mate_sem_init(&instance, "SEM4", 1);
 
-  mate_sem_wait(&instance, "SEM4");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM1");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM4");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM1");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM1");
-  mate_sem_post(&instance, "SEM4");
+    mate_sem_post(&instance, "SEM1");
+    mate_sem_post(&instance, "SEM4");
 
 	printf("C4 - Se retira a descansar\n");
 	mate_close(&instance);
@@ -107,16 +107,16 @@ void* carpincho5_func(void* config){
 	printf("C5 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM5", 1);
-  mate_sem_init(&instance, "SEM6", 1);
+    mate_sem_init(&instance, "SEM5", 1);
+    mate_sem_init(&instance, "SEM6", 1);
 
-  mate_sem_wait(&instance, "SEM5");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM6");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM5");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM6");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM5");
-  mate_sem_post(&instance, "SEM6");
+    mate_sem_post(&instance, "SEM5");
+    mate_sem_post(&instance, "SEM6");
 
 	printf("C5 - Se retira a descansar\n");
 	mate_close(&instance);
@@ -129,20 +129,20 @@ void* carpincho6_func(void* config){
 	printf("C6 - Llamo a mate_init\n");
 	mate_init(&instance, (char*)config);
 
-  mate_sem_init(&instance, "SEM5", 1);
-  mate_sem_init(&instance, "SEM6", 1);
+    mate_sem_init(&instance, "SEM5", 1);
+    mate_sem_init(&instance, "SEM6", 1);
 
-  mate_sem_wait(&instance, "SEM6");
-  sleep(1000);
-  mate_sem_wait(&instance, "SEM1");
-  sleep(1000);
-  mate_sem_post(&instance, "SEM1");
+    mate_sem_wait(&instance, "SEM6");
+    sleep(1000);
+    mate_sem_wait(&instance, "SEM1");
+    sleep(1000);
+    mate_sem_post(&instance, "SEM1");
 
-  mate_sem_wait(&instance, "SEM5");
-  sleep(1000);
+    mate_sem_wait(&instance, "SEM5");
+    sleep(1000);
 
-  mate_sem_post(&instance, "SEM5");
-  mate_sem_post(&instance, "SEM6");
+    mate_sem_post(&instance, "SEM5");
+    mate_sem_post(&instance, "SEM6");
 
 	printf("C6 - Se retira a descansar\n");
 	mate_close(&instance);
@@ -181,12 +181,12 @@ int main(int argc, char *argv[]) {
 	printf("MAIN - Como no sabemos a quienes va a matar el algoritmo, entonces hacemos el free de los semáforos acá");
 	mate_instance instance;
 	mate_init(&instance, argv[1]);
-  mate_sem_destroy(&instance, "SEM1");
-  mate_sem_destroy(&instance, "SEM2");
-  mate_sem_destroy(&instance, "SEM3");
-  mate_sem_destroy(&instance, "SEM4");
-  mate_sem_destroy(&instance, "SEM5");
-  mate_sem_destroy(&instance, "SEM6");
+    mate_sem_destroy(&instance, "SEM1");
+    mate_sem_destroy(&instance, "SEM2");
+    mate_sem_destroy(&instance, "SEM3");
+    mate_sem_destroy(&instance, "SEM4");
+    mate_sem_destroy(&instance, "SEM5");
+    mate_sem_destroy(&instance, "SEM6");
 	mate_close(&instance);
 
 	printf("MAIN - Retirados los carpinchos de la pelea, hora de analizar los hechos\n");
